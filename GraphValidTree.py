6474d3930dc7f3371ce099555c04ac0ca130b1a8
Rahul Trivedi <trivedirahul1999@gmail.com>
2024-07-07 23:54:28 -0700

    Added Graph And DFS problems

---
+++ GraphValidTree.py
@@ -0,0 +1,52 @@
+# Time & Space Complexity: O(E + V)
+# Neetcode: https://www.youtube.com/watch?v=bXsUuownnoQ
+
+class Solution:
+    def validTree(self, n, edges):
+        # A graph is a valid tree if:
+        # 1. There are no cycles
+        # 2. All nodes are connected
+
+        # If no nodes, the graph is trivially a tree
+        if not n:
+            return True
+
+        # Create an adjacency list to represent the graph
+        adj = {i: [] for i in range(n)}
+        for n1, n2 in edges:
+            # For an undirected graph, add the connection both ways
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        # A set to keep track of visited nodes
+        visit = set()
+
+        def dfs(i, prev):
+            # If we encounter a node that we've seen before, there is a cycle
+            if i in visit:
+                return False
+
+            # Mark this node as visited
+            visit.add(i)
+
+            # Check all adjacent nodes
+            for j in adj[i]:
+                # Ignore the node we came from to prevent false cycle detection
+                if j == prev:
+                    continue
+
+                # Recursively perform DFS, if a cycle is detected return False
+                if not dfs(j, i):
+                    return False
+
+            return True
+
+        # Perform DFS starting from node 0. Ensure there are no cycles
+        # and that all nodes are connected (visited).
+        return dfs(0, -1) and n == len(visit)
+
+
+c = Solution()
+n = 5
+edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+print(c.validTree(n, edges))
---
+++ WaterBottles.py
@@ -0,0 +1,25 @@
+# Time: O(logn)
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        # Initialize the count of bottles drunk to the initial number of full bottles
+        count = numBottles
+        # Initialize the number of empty bottles to the initial number of full bottles
+        empty_bottles = numBottles
+
+        # Continue the loop as long as we have enough empty bottles to exchange for at least one full bottle
+        while empty_bottles >= numExchange:
+            # Calculate the number of new full bottles we get by exchanging empty bottles
+            new_bottles = empty_bottles // numExchange
+            # Update the number of empty bottles to the remainder after exchanging, plus the new full bottles
+            empty_bottles = (empty_bottles % numExchange) + new_bottles
+            # Add the new full bottles to the count of total bottles drunk
+            count += new_bottles
+
+        # Return the total count of bottles drunk
+        return count
+
+
+c = Solution()
+numBottles = 15
+numExchange = 4
+print(c.numWaterBottles(numBottles, numExchange))
